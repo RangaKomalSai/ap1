ed55551bd939c8068c6262d97ceec9c183ced23b
RangaKomalSai <ranga.komal.sai@gmail.com>
2024-09-07 15:31:32 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* Add more routes as needed */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
